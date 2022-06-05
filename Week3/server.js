3c792bef96a8a961df78bbfbfd308d9811091d99
PHLin199988 <yk40403@gmail.com>
2022-06-05 00:19:18 -0400

    converted server to express

--- Week3/server.js
+++ Week3/server.js
@@ -1,40 +1,15 @@
-// require built-in modules
-const http = require('http'); // CommonJS Module pattern (CJS)
-const fs = require('fs'); 
-const mime = require('mime-types');
-
-let lookup = mime.lookup; // alias for mime.lookup
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-
-// when we create a server instance, it is IMMUTABLE = cannot be changed until the server is restarted
-const server = http.createServer(function(req, res)   
-{
-    let path = req.url; // alias for req.url
-
-    if(path =="/" || path =="/home")
-    {
-        path = "/index.html";
-    }
-
-    let mime_type = lookup(path.substring(1));
-    //console.log(`MIME TYPE: ${mime_type}`);
-
-   fs.readFile(__dirname + path, function(err, data)
-   {
-        if(err)
-        {
-            res.writeHead(404); // status - file not found
-            return res.end("ERROR: 404 - File Not Found"); // output the error message to the page
-        }
-        // no error
-        res.setHeader("X-Content-Type-Options", "nosniff"); // security guard
-        res.writeHead(200, {'Content-Type': mime_type}); // status - all ok
-        return res.end(data); // output the file that was read to the page
-   });
+app.get('/', function (req, res) {
+    res.send('Hello, World!');
 });
-
-server.listen(port, function() 
-{
-    console.log(`Server running at Port: ${port}`);
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}`);
 });
+//# sourceMappingURL=server.js.map
