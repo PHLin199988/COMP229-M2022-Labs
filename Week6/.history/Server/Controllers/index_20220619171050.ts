6e000c8a225849c5c4c895bcfbf3b6a0af834609
PHLin199988 <yk40403@gmail.com>
2022-06-19 17:13:47 -0400

    configured the user to be detected

--- Week6/.history/Server/Controllers/index_20220619171050.ts
+++ Week6/.history/Server/Controllers/index_20220619171050.ts
@@ -4,25 +4,25 @@ import { UserDisplayName } from '../Util';
 
 export function DisplayHomePage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Home', page: 'home', displayName: UserDisplayName(req)});
+    res.render('index', { title: 'Home', page: 'home', displayName: '' });
 }
 
 export function DisplayAboutPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'About Us', page: 'about', displayName: UserDisplayName(req)});
+    res.render('index', { title: 'About Us', page: 'about', displayName: ''  });
 }
 
 export function DisplayProjectsPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Projects', page: 'projects', displayName: UserDisplayName(req)});
+    res.render('index', { title: 'Our Projects', page: 'projects', displayName: ''  });
 }
  
 export function DisplayServicesPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Services', page: 'services', displayName: UserDisplayName(req)});
+    res.render('index', { title: 'Our Services', page: 'services', displayName: ''  });
 }
 
 export function DisplayContactPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Contact Us', page: 'contact', displayName: UserDisplayName(req)});
+    res.render('index', { title: 'Contact Us', page: 'contact', displayName: ''  });
 }
