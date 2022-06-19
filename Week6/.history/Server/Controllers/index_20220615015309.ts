e329e7942acc92b657c7fb4eca0062cb4167e795
PHLin199988 <yk40403@gmail.com>
2022-06-19 15:54:39 -0400

    modified Navbar for auth

--- Week6/.history/Server/Controllers/index_20220615015309.ts
+++ Week6/.history/Server/Controllers/index_20220615015309.ts
@@ -2,25 +2,25 @@ import express from 'express';
 
 export function DisplayHomePage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Home', page: 'home', displayName: '' });
+    res.render('index', { title: 'Home', page:'home' });
 }
 
 export function DisplayAboutPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'About Us', page: 'about', displayName: ''  });
+res.render('index', { title: 'About Me', page:'about' });
 }
 
 export function DisplayProjectsPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Projects', page: 'projects', displayName: ''  });
+    res.render('index', { title: 'My Projects', page:'projects' });
 }
- 
+
 export function DisplayServicesPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Our Services', page: 'services', displayName: ''  });
+    res.render('index', { title: 'My Services', page:'services' });
 }
 
 export function DisplayContactPage(req: express.Request, res: express.Response, next: express.NextFunction)
 {
-    res.render('index', { title: 'Contact Us', page: 'contact', displayName: ''  });
+    res.render('index', { title: 'Contact Us', page:'contact' });
 }
